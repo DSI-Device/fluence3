baf90aa7e3495845519a44160032249f33f2a5f7
Nahid Rahman <Shuvo@Tanveer-Hasans-iMac.local>
2013-03-22 19:50:18 +0600

    Initial Commit

---
+++ Fluence3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fluence3
+//
+//  Created by Joseph Armand Baroi on 3/22/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fluence3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fluence3
+//
+//  Created by Joseph Armand Baroi on 3/22/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
