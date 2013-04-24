9731dc0a8e6da0380965276d88ed8d0e55160d7c
Mahfuz <shuvo.mahfuz@gmail.com>
2013-04-24 18:16:40 +0600

    a

--- Fluence3/MapViewController.m
+++ Fluence3/MapViewController.m
@@ -52,20 +52,6 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
-    NSDictionary *user1 = [[NSDictionary  alloc] initWithObjectsAndKeys: @"1", @"userId", @"Naim", @"userName", @"51.5099", @"userLatitude", @"51.5099", @"userLontitude",@"1", @"userCat", @"http://farm6.static.flickr.com/5042/5323996646_9c11e1b2f6_b.jpg", @"userPic",nil];
-    NSDictionary *user2 = [[NSDictionary  alloc] initWithObjectsAndKeys: @"2", @"userId", @"Nazmul", @"userName", @"51.5099", @"userLatitude", @"51.5099", @"userLontitude",@"1", @"userCat", @"http://farm6.static.flickr.com/5042/5323996646_9c11e1b2f6_b.jpg", @"userPic", nil];
-    
-    CLLocationCoordinate2D location;
-    location.latitude = 37.33182;
-    location.longitude = -122.03118;
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location, 500, 500);
-    [self.mapView setRegion:region animated:YES];
-    
-    MKPointAnnotation *annotation = [[MKPointAnnotation alloc]autorelease];
-    annotation.coordinate = location;
-    annotation.title = @"test Title";
-    annotation.subtitle = @"testing";
-    [self.mapView addAnnotation:annotation];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
